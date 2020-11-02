a445830936262fc47992500172be28201e4bd8a1
Salik G <salikvilmer@gmail.com>
2020-11-02 17:35:57 +0100

    small change of one name

--- public/js/app.js
+++ public/js/app.js
@@ -1913,7 +1913,7 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 /* harmony default export */ __webpack_exports__["default"] = ({
-  name: "3dModelRenderer"
+  name: "ModelRenderer"
 });
 
 /***/ }),
--- resources/assets/js/components/ModelRenderer.vue
+++ resources/assets/js/components/ModelRenderer.vue
@@ -4,7 +4,7 @@
 
 <script>
 export default {
-name: "3dModelRenderer"
+name: "ModelRenderer"
 }
 </script>
 
